9378c505353a5fffc4d562762f6858841b260e92
Chat Application <you@example.com>
2025-05-07 22:53:00 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Auth/login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboear";
+import { getLocalStrage, setLocalStorage } from "./utils/localStorage";
+import { useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState();
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      setUser("admin");
+    } else if (email == "user@me.com" && password == "123") {
+      setUser("employee");
+    } else {
+      alert("Invailid credential");
+    }
+  };
+
   return (
     <>
-      <AdminDashboard />
+      {!user ? <Login handleLogin={handleLogin} /> : " "}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   );
 };
